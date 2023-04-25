d6aaeca0ebf12afd6567eb6c071e547e77462caf
fitodac <fitodac@gmail.com>
2023-04-25 14:59:15 -0300

    Setup de tailwind, google fonts

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+			colors: {
+				brand: {
+					cream: '#E3DCBF',
+					brown: '#BDB689',
+					olive: '#787B34',
+					'light-green': '#68850E',
+					green: '#2B4B1B',
+					yellow: '#FFBA00'
+				}
+			}
+		},
+  },
+  plugins: [],
+}
+
