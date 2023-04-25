2349fa75c77a22f14af87e603c0084c8748180d8
Marco <tignanellimarco@gmail.com>
2023-04-25 18:03:18 -0300

    bases definidas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
-  content: [],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
-			fontFamily: {
-				poppins: 'Poppins, times new roman, sans-serif'
-			},
-			colors: {
-				brand: {
-					cream: '#E3DCBF',
-					brown: '#BDB689',
-					olive: '#787B34',
-					'light-green': '#68850E',
-					green: '#2B4B1B',
-					yellow: '#FFBA00'
-				}
-			}
-		},
+      fontFamily: {
+        poppins: 'Poppins, times new roman, sans-serif'
+      },
+      colors: {
+        brand: {
+          cream: '#E3DCBF',
+          brown: '#BDB689',
+          olive: '#787B34',
+          'light-green': '#68850E',
+          green: '#2B4B1B',
+          yellow: '#FFBA00'
+        }
+      }
+    },
   },
   plugins: [],
 }
 
-=======
-    content: [
-        "./src/pages/**/*.{js,ts,jsx,tsx}",
-        "./src/components/**/*.{js,ts,jsx,tsx}",
-        "./src/app/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-};
->>>>>>> 6a7b4c98acb3e607fffc5613aef14c00aa234fa5
