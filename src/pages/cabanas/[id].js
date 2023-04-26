ab1bac6c622e51fed4c901571e65558fc74c5ffe
Marco <93273771+Mark-kus@users.noreply.github.com>
2023-04-26 17:17:23 -0300

    console logs comentados, componente arriba

--- src/pages/cabanas/[id].js
+++ src/pages/cabanas/[id].js
@@ -1,5 +1,14 @@
 import { supabase } from "utils/supabase";
 
+export default function Room({ room }) {
+ // console.log(room);
+  return (
+    <div>
+      <h1>{room.id}</h1>
+    </div>
+  );
+}
+
 export async function getServerSideProps({ params }) {
   const { id } = params;
 
@@ -10,8 +19,8 @@ export async function getServerSideProps({ params }) {
   // .single();
 
   if (error) {
-    console.log(`Error al obtener el room con uuid ${id}`);
-    console.log(error);
+   // console.log(`Error al obtener el room con uuid ${id}`);
+   // console.log(error);
     return {
       notFound: true,
     };
@@ -22,13 +31,4 @@ export async function getServerSideProps({ params }) {
       room: room[0],
     },
   };
-}
-
-export default function Room({ room }) {
-  console.log(room);
-  return (
-    <div>
-      <h1>{room.id}</h1>
-    </div>
-  );
 }
