485fe0f65eca10dfc37d351b2b79cf9efd6eb635
Tomas030284 <tomas.mogrovejo.acosta@gmail.com>
2023-04-27 08:41:14 -0500

    Create controlers, handlers and index to get, post, put and delete Comments

---
+++ src/pages/api/comments/controllers.js
@@ -0,0 +1,58 @@
+import { supabase } from "../../../utils/supabase";
+
+export const getAllComments = async () => {
+  const { data: comments, error } = await supabase.from("comments").select();
+
+  if (error) {
+    console.log(error);
+    throw error;
+  }
+
+  return comments;
+};
+
+export const postNewComment = async ({ review, user_id, stars, approved }) => {
+  const { data: postComment, error } = await supabase
+    .from("comments")
+    .insert([
+      {
+        review,
+        user_id,
+        stars,
+        approved: false,
+      },
+    ])
+    .select();
+  if (error) {
+    throw error;
+  }
+  return postComment;
+};
+
+export const updateComment = async ({
+  id,
+  review,
+  user_id,
+  stars,
+  approved,
+}) => {
+  const { data: updateComments, error } = await supabase
+    .from("comments")
+    .update({ review, user_id, stars, approved })
+    .eq("id", id)
+    .select();
+  if (error) {
+    throw error;
+  }
+  return updateComments;
+};
+
+export async function deleteComment ({ id }) {
+  const { data, error } = await supabase.from("comments").delete().eq("id", id);
+
+  if (error) {
+    console.log(error);
+    throw error;
+  }
+  return data;
+};
