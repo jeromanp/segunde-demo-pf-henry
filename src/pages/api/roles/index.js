13627b7b2686b8195c0638397426baa6605e1f9f
Tomas030284 <tomas.mogrovejo.acosta@gmail.com>
2023-04-27 15:56:59 -0500

    Create index, controllers, handlers to get, post, put, delete roles

---
+++ src/pages/api/roles/index.js
@@ -0,0 +1,20 @@
+import * as Methods from "../methods";
+import * as Handlers from "./handlers";
+
+export default async function handlerRoles(req, res) {
+  const method = req.method;
+  switch (method) {
+    case Methods.GET:
+      return await Handlers.handlerGet(req, res);
+    case Methods.POST:
+      return await Handlers.handlerPost(req, res);
+    case Methods.PUT:
+      return await Handlers.handlerPut(req, res);
+    case Methods.DELETE:
+      return await Handlers.handlerDelete(req, res);
+    default:
+      res.status(400).json({
+        message: "400 Bad Request : invalid method",
+      });
+  }
+}
