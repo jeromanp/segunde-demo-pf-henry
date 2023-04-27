7a79b98609cbde1ad5ebb42121d0c2d135b6f70f
jestruvec <jhonnyestruve@gmail.com>
2023-04-27 14:44:22 -0600

    creacion de ruta /api/cabana/ post put delete

--- src/pages/api/cabanas/controllers.js
+++ src/pages/api/cabanas/controllers.js
@@ -1,24 +1,128 @@
 import { supabase } from "utils/supabase";
 import { filterRoomsByCapacity } from "helpers/filters";
 
-const cabanasControllers = {};
+//const cabanasControllers = {};
 
 //Retorna todas las cabanas.
 //En caso de que exista el query params capacity retorna las cabanas filtradas por capacidad
-cabanasControllers.get = async (query) => {
-    const { data: rooms, error } = await supabase.from("rooms").select("*");
-    if (error) {
-        console.log(error);
-        return { data: null, error };
-    }
-    if (query.capacity !== undefined) {
-        return {
-            data: filterRoomsByCapacity(rooms, parseInt(query.capacity)),
-            error: null,
-        };
-    } else {
-        return { data: rooms, error: null };
-    }
+
+// cabanasControllers.get = async (query) => {
+//     const { data: rooms, error } = await supabase.from("rooms").select("*");
+//     if (error) {
+//         console.log(error);
+//         return { data: null, error };
+//     }
+//     if (query.capacity !== undefined) {
+//         return {
+//             data: filterRoomsByCapacity(rooms, parseInt(query.capacity)),
+//             error: null,
+//         };
+//     } else {
+//         return { data: rooms, error: null };
+//     }
+// };
+
+//GET
+
+export const getAllRooms = async (query) => {
+  const { data: rooms, error } = await supabase.from("rooms").select();
+  if (error) {
+    throw error;
+  }
+  if (query.capacity !== undefined) {
+    return {
+      data: filterRoomsByCapacity(rooms, parseInt(query.capacity)),
+      error: null,
+    };
+  }
+  return rooms;
 };
 
-export default cabanasControllers;
+//POST
+
+export const postRoom = async ({
+  name,
+  type,
+  rooms,
+  services,
+  price,
+  capacity,
+  gallery_id,
+  beds,
+  bathrooms,
+  description,
+}) => {
+  const { data: postRoom, error } = await supabase
+    .from("rooms")
+    .insert([
+      {
+        name,
+        type,
+        rooms,
+        services,
+        price,
+        capacity,
+        gallery_id,
+        beds,
+        bathrooms,
+        description,
+      },
+    ])
+    .select();
+  if (error) {
+    throw error;
+  }
+  return postRoom;
+};
+
+//UPDATE
+
+export const upRoom = async ({
+  id,
+  name,
+  type,
+  rooms,
+  services,
+  price,
+  capacity,
+  gallery_id,
+  beds,
+  bathrooms,
+  description,
+}) => {
+  const { data: upRoom, error } = await supabase
+    .from("rooms")
+    .update({
+      name,
+      type,
+      rooms,
+      services,
+      price,
+      capacity,
+      gallery_id,
+      beds,
+      bathrooms,
+      description,
+    })
+    .eq("id", id)
+    .select();
+  if (error) {
+    throw error;
+  }
+  return upRoom;
+};
+
+//DELETE
+
+export const deleteRoom = async ({ id }) => {
+  const { data: delRoom, error } = await supabase
+    .from("rooms")
+    .delete()
+    .eq("id", id);
+  if (error) {
+    throw error;
+  }
+  return delRoom;
+};
+
+//export default cabanasControllers;
--- src/pages/api/cabanas/index.js
+++ src/pages/api/cabanas/index.js
@@ -1,17 +1,30 @@
-import cabanasControllers from "./controllers.js";
+//import cabanasControllers from "./controllers.js";
+import * as Handlers from "./handlers";
+import * as Methods from "../methods";
 
 export default async function handler(req, res) {
-    switch (req.method) {
-        case "GET":
-            const { data, error } = await cabanasControllers.get(req.query);
-            if (error) {
-                res.status(404).json({ error: error.message });
-            } else {
-                res.status(200).send(data);
-            }
-            break;
-        default:
-            res.status(405).json({ error: "Method not allowed" });
-            break;
-    }
+  const method = req.method;
+  switch (method) {
+    // case Methods.GET:
+    //   const { data, error } = await cabanasControllers.get(req.query);
+    //   if (error) {
+    //     res.status(404).json({ error: error.message });
+    //   } else {
+    //     res.status(200).send(data);
+    //   }
+    //   break;
+    case Methods.GET:
+      return await Handlers.handlerGet(req, res);
+    case Methods.POST:
+      return await Handlers.handlerPost(req, res);
+    case Methods.PUT:
+      return await Handlers.handlerPut(req, res);
+    case Methods.DELETE:
+      return await Handlers.handlerDelete(req, res);
+
+    default:
+      res.status(400).json({
+        message: "400 Bad Request : invalid method",
+      });
+  }
 }
