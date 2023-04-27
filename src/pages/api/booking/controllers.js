a6ab97ef0de4473320f44cdb2473a144a545b756
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2023-04-26 19:03:33 -0500

    create controller postBooking

--- src/pages/api/booking/controllers.js
+++ src/pages/api/booking/controllers.js
@@ -1,12 +1,41 @@
-import {supabase} from "../../../utils/supabase"
+import { supabase } from "../../../utils/supabase";
 
 export const getAllBooking = async () => {
-    const { data: infoBooking, error } = await supabase.from("booking").select();
+  const { data: infoBooking, error } = await supabase.from("booking").select();
 
   if (error) {
     console.log(error);
     throw error;
   }
-  console.log(infoBooking)
+  console.log(infoBooking);
   return infoBooking;
-}
+};
+
+export const postNewBooking = async ({
+  checkin,
+  checkout,
+  user_id,
+  room_id,
+  payments,
+  adults,
+  children,
+}) => {
+  const { data: postBooking, error } = await supabase
+    .from("booking")
+    .insert([
+      {
+        checkin,
+        checkout,
+        user_id,
+        room_id,
+        payments,
+        adults,
+        children,
+      },
+    ])
+    .select();
+  if (error) {
+    throw error;
+  }
+  return postBooking;
+};
