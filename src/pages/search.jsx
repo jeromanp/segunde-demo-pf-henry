b8e19e2be3ff807f6b90883180e4ce46618442f9
Lafsitoo <fretteleandro@live.com>
2023-04-28 06:59:15 -0300

    page completed, need to add date filter

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import CardCabin from "components/seeAllCabins/CardCabin";
+import Header from "components/Header";
+import InputCabin from "components/seeAllCabins/InputCabin";
+import Datepicker from "components/form/Datepicker";
 
-function search() {
+export default function Search() {
   const router = useRouter();
   const { adults, children } = router.query;
   useEffect(() => {
@@ -39,35 +42,79 @@ function search() {
   };
 
   return (
-    <div>
-      <div>
-        <h1>Search</h1>
-      </div>
-      <label htmlFor="capacityFilter">Capacidad</label>
-      <input
-        type="number"
-        value={capacity}
-        onChange={changeHandler}
-        className="border-2"
-        id="capacityFilter"
-      />
-      <button onClick={searchHandler} className="btn-yellow">
-        Search
-      </button>
-      <div className="max-h-screen overflow-y-auto">
-        <h1 className="text-brand-green text-xl font-semibold mb-4 pt-8">
-          Cabañas disponibles
-        </h1>
-        <div className="grid grid-cols-1 gap-4">
-          {rooms.map((room) => {
-            return (
-              <CardCabin key={`${room.id}-index`} cabin={room} />
-            );
-          })}
+    <>
+      <Header />
+      <div className="flex h-screen">
+        <section className="p-14">
+          <div className="bg-brand-olive rounded-2xl p-8 w-96">
+            <h3 className="text-white text-xl font-bold pb-6 pt-4">
+              Seleccionar fechas
+            </h3>
+            <div className="flex flex-col gap-2">
+              <div>
+                <label
+                  htmlFor="arrival-date"
+                  className="text-white block font-normal text-sm mb-1.5"
+                >
+                  Llegada
+                </label>
+                <div className="p-0.5 w-full rounded-xl bg-white mb-3">
+                  <Datepicker />
+                </div>
+              </div>
+              <div>
+                <label
+                  htmlFor="departure-date"
+                  className="text-white block font-normal text-sm mb-1.5"
+                >
+                  Hasta
+                </label>
+                <div className="p-0.5 w-full rounded-xl bg-white mb-3">
+                  <Datepicker />
+                </div>
+              </div>
+              <div>
+                <label
+                  htmlFor="guests"
+                  className="text-white block font-normal text-sm mb-1.5"
+                >
+                  Cant. de personas
+                </label>
+                <input
+                  type="number"
+                  id="guests"
+                  name="guests"
+                  min="1"
+                  max="10"
+                  className="text-neutral-400 border-2 p-2 w-full rounded-md"
+                  value={capacity}
+                  onChange={changeHandler}
+                />
+              </div>
+              <div className="mx-auto">
+                <button
+                  onClick={searchHandler}
+                  className="bg-brand-yellow text-white rounded-md py-2 px-4 mt-6"
+                >
+                  Buscar
+                </button>
+              </div>
+            </div>
+          </div>
+        </section>
+        <div className="flex-grow">
+          <h1 className="text-brand-green text-xl font-semibold pb-2 pt-12">
+            Cabañas disponibles
+          </h1>
+          <div className="overflow-scroll pr-12 h-4/6">
+            <div className="grid grid-cols-1 gap-2">
+              {rooms.map((room) => {
+                return <CardCabin key={`${room.id}-index`} cabin={room} />;
+              })}
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
-
-export default search;
