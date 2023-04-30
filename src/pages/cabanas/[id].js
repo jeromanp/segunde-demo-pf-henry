5aa8438b06e48b54711bf76c84cb7172295f80ee
Marco <tignanellimarco@gmail.com>
2023-04-29 23:54:39 -0300

    id slider basis

--- src/pages/cabanas/[id].js
+++ src/pages/cabanas/[id].js
@@ -1,14 +1,33 @@
 import { supabase } from "utils/supabase";
 import Layout from "../../layouts/Layout.jsx";
 import Link from "next/link";
+import Slider from "react-slick";
 
 export default function Room({ room }) {
     const description = room.description.replace(/,|\./g, "");
     // console.log(room);
+
+    const settings = {
+        customPaging: function (i) {
+            return (
+                <a>
+                    {/* <img src='' className={`w-5 h-5 bg-red-${i + 3}00`} /> */}
+                    <div className="bg-slate-500 rounded-lg text-slate-500">-</div>
+                </a>
+            );
+        },
+        dots: true,
+        dotsClass: "slick-dots slick-thumb",
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    };
+
     return (
         <Layout>
-            <div className="flex flex-col lg:flex-row justify-center mx-auto max-w-7xl py-10 lg:px-10">
-                <div className="lg:w-1/2 lg:pr-10 mb-10 lg:mb-0 text-brand-green">
+            <div className="flex justify-center mx-auto max-w-7xl py-10 lg:px-10">
+                <div className="lg:w-1/2 lg:pr-10 mb-10 lg:mb-0 text-brand-green m-auto">
                     <h2 className=" text-4xl font-bold mb-4">{room.name}</h2>
                     <div>
                         <ul className="flex -ml-16 max-w-fit my-4 pl-4 py-2 rounded-xl bg-gray-50 shadow-lg">
@@ -27,16 +46,23 @@ export default function Room({ room }) {
                         </Link>
                     </div>
                 </div>
-                <div className="lg:w-1/2 flex flex-col">
-                    <div className="max-w-full w-[600px] h-[450px] bg-slate-400 rounded-2xl"></div>
-                    <div className="flex mt-4 max-w-full gap-4">
-                        <div className="max-w-full w-1/4 h-24 bg-slate-400"></div>
-                        <div className="max-w-full w-1/4 h-24 bg-slate-400"></div>
-                        <div className="max-w-full w-1/4 h-24 bg-slate-400"></div>
-                        <div className="max-w-full w-1/4 h-24 bg-slate-400"></div>
-                    </div>
+                <div className="w-1/3 m-auto">
+                    <Slider {...settings}>
+                        {/* {[...Array(4)].map((_, i) => ( */}
+                        <div className="h-96 bg-slate-200 text-slate-200 rounded-xl">.</div>
+                        <div className="h-96 bg-slate-400 text-slate-400 rounded-xl">.</div>
+                        <div className="h-96 bg-slate-600 text-slate-600 rounded-xl">.</div>
+                        <div className="h-96 bg-slate-800 text-slate-800 rounded-xl">.</div>
+                        {/* ))} */}
+                    </Slider>
                 </div>
             </div>
+            <style>{`
+            .slick-thumb li {
+                width: 24%;
+                margin: 0 0.50%;
+            }
+            `}</style>
         </Layout>
     );
 }
