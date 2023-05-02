704c8135bea696a1101380387fa59947212d4777
Lafsitoo <fretteleandro@live.com>
2023-05-02 08:18:46 -0300

    Fix

--- src/components/seeAllCabins/CardCabin.jsx
+++ src/components/seeAllCabins/CardCabin.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function CardCabin({ cabin }) {
     return (
-        <div className="border rounded-2xl border-2 overflow-hidden shadow-md flex flex-col md:flex-row m-1">
+        <div className="border rounded-2xl overflow-hidden shadow-lg flex flex-col md:flex-row m-1">
             <div className="relative w-80 h-56 flex-shrink-0 p-4">
                 <img
                     src="https://via.placeholder.com/500x500.png?text=Image+Not+Found"
@@ -12,14 +12,14 @@ export default function CardCabin({ cabin }) {
                     className="border rounded-2xl object-cover w-full h-full"
                 />
             </div>
-            <div className="pl-8 pt-6">
+            <div className="pl-8 pr-4 pt-6">
                 <h2 className="text-brand-green text-2xl font-semibold mb-4 hover:underline">
                     <Link href={`/cabanas/${cabin.id}`}>{cabin.name}</Link>
                 </h2>
                 <div className="text-lg mb-0.5">
                     <i className="ri-user-3-fill text-gray-500 mr-2"></i>
                     <span className="text-gray-800 font-normal">
-                        Cabaña de {cabin.rooms} habitaciones para{" "}
+                        Cabaña de {cabin.rooms} habitaciones para
                         {cabin.capacity} personas
                     </span>
                 </div>
