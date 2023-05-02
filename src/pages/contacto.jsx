0ac9ec1d450fa44939222999016a095da37d8fa7
Lafsitoo <fretteleandro@live.com>
2023-05-02 08:18:24 -0300

    padding

--- src/pages/contacto.jsx
+++ src/pages/contacto.jsx
@@ -6,15 +6,15 @@ export default function Contact() {
   return (
     <>
       <Header />
-      <section className="px-4 py-8">
+      <section className="px-4 py-8 pb-16">
           <div className="flex mt-6 justify-around">
-            <div className="w-1/3">
+            <div className="w-1/3 pb-8">
               <h2 className="text-brand-green text-4xl font-bold mb-6 mt-4">
                 Contactanos!
               </h2>
               <ContactForm />
             </div>
-            <img src="/contact.svg" alt="contact.icon" className="w-1/2 h-auto mr-4" />
+            <img src="/contact.svg" alt="contact.icon" className="w-2/5 mr-4" />
           </div>
       </section>
       <Footer />
