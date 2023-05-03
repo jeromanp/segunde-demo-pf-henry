15e8b50e9e9ad1534e1598c5a1a0cd365adff25d
jestruvec <jhonnyestruve@gmail.com>
2023-05-03 13:11:08 -0600

    back end 101: crear ruta dinamica api/booking/[id]

--- src/pages/api/booking/handlers.js
+++ src/pages/api/booking/handlers.js
@@ -1,41 +1,51 @@
-import * as Controllers from "./controllers"
+import * as Controllers from "./controllers";
 
-export async function handlerGet (req, res){
-    try {
-        const booking= await Controllers.getAllBooking()
-        return res.status(200).json(booking)
-    } catch (error) {
-        return res.status(500).json({ error: error.message });
-    }
+export async function handlerGet(req, res) {
+  try {
+    const booking = await Controllers.getAllBooking();
+    return res.status(200).json(booking);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+}
+
+export async function handlerGetById(req, res) {
+  const { id } = req.query;
+  try {
+    const room = await Controllers.getBookingById(id);
+    return res.status(200).json(room);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 }
 
 export async function handlerPost(req, res) {
-    const body = req.body;
-    try {
-      const postBooking = await Controllers.postNewBooking(body);
-      return res.status(200).json(postBooking);
-    } catch (error) {
-      return res.status(404).json({ error: error.message });
-    }
+  const body = req.body;
+  try {
+    const postBooking = await Controllers.postNewBooking(body);
+    return res.status(200).json(postBooking);
+  } catch (error) {
+    return res.status(404).json({ error: error.message });
   }
+}
 
-  export async function handlerPut(req, res) {
-    const body = req.body;
-    try {
-      const updateBooking = await Controllers.updateBooking(body);
-      return res.status(200).json(updateBooking);
-    } catch (error) {
-      return res.status(404).json({ error: error.message });
-    }
+export async function handlerPut(req, res) {
+  const body = req.body;
+  try {
+    const updateBooking = await Controllers.updateBooking(body);
+    return res.status(200).json(updateBooking);
+  } catch (error) {
+    return res.status(404).json({ error: error.message });
   }
+}
 
-  export async function handlerDelete(req, res) {
-    const body = req.body;
-  
-    try {
-      const response = await Controllers.deleteBooking(body);
-      return res.status(200).json({ message: "Booking successfully removed" });
-    } catch (error) {
-      return res.status(500).json({ error: error.message });
-    }
+export async function handlerDelete(req, res) {
+  const body = req.body;
+
+  try {
+    const response = await Controllers.deleteBooking(body);
+    return res.status(200).json({ message: "Booking successfully removed" });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
   }
+}
