83b279bca85151957e73e36801187f2247291741
fitodac <fitodac@gmail.com>
2023-05-04 13:17:35 -0300

    componentes del dashboard

---
+++ src/components/dashboard/tables/TableHead.jsx
@@ -0,0 +1,16 @@
+export default function TableHead({data})
+{
+	return (
+		<thead>
+			<tr className="bg-gray-2 text-left">
+				{data.map(e => (
+					<th 
+						key={e.idx}
+						className={`text-sm min-w-[${e.width}] py-4 px-4 font-semibold text-black`}>
+						{ e.title }
+					</th>
+				))}
+			</tr>
+		</thead>
+	)
+}
