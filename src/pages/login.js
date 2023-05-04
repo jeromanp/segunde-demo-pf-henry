3bc73ab9e2cd1913a1aba8fb1e3dae145b252ce3
calli97 <nahuelcallalli@gmail.com>
2023-05-04 16:18:58 -0300

    checkout solo para usuarios logueados

--- src/pages/login.js
+++ src/pages/login.js
@@ -6,27 +6,28 @@ import Header from "components/Header";
 import Footer from "components/Footer";
 
 const Login = () => {
-  const session = useSession();
-  const supabase = useSupabaseClient();
-  // console.log("SESSION", session);
-  return (
-    <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-     <Header /> 
-      {!session ? (
-        <Auth
-          redirectTo="http://localhost:3000/"
-          providers={["github", "google", "facebook"]}
-          supabaseClient={supabase}
-          appearance={{ theme: ThemeSupa }}
-          theme="white"
-          socialLayout="horizontal"
-        />
-      ) : (
-        <Account session={session} />
-      )}
-      <Footer />
-    </div>
-  );
+    const session = useSession();
+    const supabase = useSupabaseClient();
+    // console.log("SESSION: ", session);
+    // console.log("Supabase???: ", supabase);
+    return (
+        <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+            <Header />
+            {!session ? (
+                <Auth
+                    redirectTo="http://localhost:3000/"
+                    providers={["github", "google", "facebook"]}
+                    supabaseClient={supabase}
+                    appearance={{ theme: ThemeSupa }}
+                    theme="white"
+                    socialLayout="horizontal"
+                />
+            ) : (
+                <Account session={session} />
+            )}
+            <Footer />
+        </div>
+    );
 };
 
 export default Login;
