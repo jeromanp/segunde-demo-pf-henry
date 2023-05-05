3ae05def877dba4eea700d3c8afe3f490d2658ab
jestruvec <jhonnyestruve@gmail.com>
2023-05-05 17:00:49 -0600

    front end 68: deshardcodeo del listado de cabanas, back end 129: fix api/cabanas/[id]

--- src/pages/admin/rooms/index.jsx
+++ src/pages/admin/rooms/index.jsx
@@ -1,79 +1,119 @@
-import Layout from '../../../layouts/DashboardLayout'
-import Header from '../../../components/dashboard/PageHeader'
-import TableHead from '../../../components/dashboard/tables/TableHead'
-import Link from 'next/link'
+import Layout from "../../../layouts/DashboardLayout";
+import Header from "../../../components/dashboard/PageHeader";
+import TableHead from "../../../components/dashboard/tables/TableHead";
+import Link from "next/link";
+import { supabase } from "utils/supabase";
+import axios from "axios";
+import { useState } from "react";
 
 const table_head = [
-	{ idx: 'image', title: '' },
-	{ idx: 'cabin', title: 'Cabaña', width: '220px' },
-	{ idx: 'total-booking-2023', title: 'Reservas totales (2023)', width: '150px' },
-	{ idx: 'actions', title: 'Acciones' },
-]
+  { idx: "image", title: "" },
+  { idx: "cabin", title: "Cabaña", width: "220px" },
+  {
+    idx: "total-booking-2023",
+    title: "Reservas totales (2023)",
+    width: "150px",
+  },
+  { idx: "actions", title: "Acciones" },
+];
 
+export default function Dashboard({ rooms }) {
+  const [roomList, setRoomList] = useState(rooms);
 
-export default function Dashboard()
-{
+  const handleDelete = async (id) => {
+    const confirmation = window.confirm(
+      "¿Está seguro que desea eliminar este registro?"
+    );
+    if (confirmation) {
+      await axios.delete(`/api/cabanas/${id}`);
+      alert(`La cabana ha sido eliminada correctamente`);
+      setRoomList(roomList.filter((room) => room.id !== id));
+    }
+  };
 
-	return (
-		<Layout>
-			
-			<Header 
-				title="Cabañas"
-				breadcrumbs={(<>
-					<li>/</li>
-					<li className="text-primary">Cabañas</li>
-				</>)}>
-				
-				<Link 
-					href="/admin/rooms/create"
-					className="inline-flex items-center justify-center rounded-md bg-primary bg-opacity-70 py-1.5 px-4 text-sm text-center font-medium text-white hover:bg-opacity-90">
-					Nueva cabaña
-				</Link>
-			</Header>
+  return (
+    <Layout>
+      <Header
+        title="Cabañas"
+        breadcrumbs={
+          <>
+            <li>/</li>
+            <li className="text-primary">Cabañas</li>
+          </>
+        }
+      >
+        <Link
+          href="/admin/rooms/create"
+          className="inline-flex items-center justify-center rounded-md bg-primary bg-opacity-70 py-1.5 px-4 text-sm text-center font-medium text-white hover:bg-opacity-90"
+        >
+          Nueva cabaña
+        </Link>
+      </Header>
 
+      <div className="flex flex-col gap-10">
+        <div className="rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default  sm:px-7.5 xl:pb-1">
+          <div className="max-w-full overflow-x-auto">
+            <table className="w-full table-auto">
+              <TableHead data={table_head} />
 
-			<div className="flex flex-col gap-10">
-				<div
-					className="rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default  sm:px-7.5 xl:pb-1">
-					<div className="max-w-full overflow-x-auto">
-						
-						<table className="w-full table-auto">
-							<TableHead data={table_head} />
-							
-							<tbody>
-								{[1,2,3,4,5,6,7,8,9,10].map(e => (
-									<tr key={e}>
-										<td className="border-b border-[#eee] py-5 px-4">
-											<div className="h-12.5 w-15 rounded-md">
-												<img src="../product-04.png" alt="Product" />
-											</div>
-										</td>
-										<td className="border-b border-[#eee] py-5 px-4">
-											<h5 className="font-medium text-black">Cabaña A</h5>
-											<p className="text-sm">La mas mejor</p>
-										</td>
-										<td className="border-b border-[#eee] py-5 px-4">
-											<p className="text-black">1322</p>
-										</td>
-										<td className="border-b border-[#eee] py-5 px-4">
-											<div className="flex items-center space-x-3.5">
-												<a className="hover:text-primary" href="/admin/rooms/edit">
-													<i className="ri-edit-line text-xl leading-none"></i>
-												</a>
-												<button className="hover:text-primary">
-													<i className="ri-close-circle-line text-xl leading-none"></i>
-												</button>
-											</div>
-										</td>
-									</tr>
-								))}
+              <tbody>
+                {roomList &&
+                  roomList.map((room) => (
+                    <tr key={room.id}>
+                      <td className="border-b border-[#eee] py-5 px-4">
+                        <div className="h-12.5 w-15 rounded-md">
+                          <img src="../product-04.png" alt="Product" />
+                        </div>
+                      </td>
+                      <td className="border-b border-[#eee] py-5 px-4">
+                        <h5 className="font-medium text-black">{room.name}</h5>
+                        <p className="text-sm">Disponible?</p>
+                      </td>
+                      <td className="border-b border-[#eee] py-5 px-4">
+                        <p className="text-black">estadisticas?</p>
+                      </td>
+                      <td className="border-b border-[#eee] py-5 px-4">
+                        <div className="flex items-center space-x-3.5">
+                          <a
+                            className="hover:text-primary"
+                            href={`/admin/rooms/edit?id=${room.id}`}
+                          >
+                            <i className="ri-edit-line text-xl leading-none"></i>
+                          </a>
+                          <button
+                            onClick={() => handleDelete(room.id)}
+                            className="hover:text-primary"
+                          >
+                            <i className="ri-close-circle-line text-xl leading-none"></i>
+                          </button>
+                        </div>
+                      </td>
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
 
-							</tbody>
-						</table>
+export async function getServerSideProps() {
+  const { data: rooms, error } = await supabase
+    .from("rooms")
+    .select("*")
+    .order("name", { ascending: true });
 
-					</div>
-				</div>
-			</div>
-		</Layout>
-	)
+  if (error) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      rooms: rooms,
+    },
+  };
 }
--- src/pages/api/cabanas/[id]/index.js
+++ src/pages/api/cabanas/[id]/index.js
@@ -2,13 +2,15 @@ import * as Handlers from "../handlers";
 import * as Methods from "../../methods";
 
 export default async function handler(req, res) {
-    const method = req.method;
-    switch (method) {
-        case Methods.GET:
-            return await Handlers.handlerGetById(req, res);
-        default:
-            res.status(400).json({
-                message: "400 Bad Request : invalid method",
-            });
-    }
+  const method = req.method;
+  switch (method) {
+    case Methods.GET:
+      return await Handlers.handlerGetById(req, res);
+    case Methods.DELETE:
+      return await Handlers.handlerDelete(req, res);
+    default:
+      res.status(400).json({
+        message: "400 Bad Request : invalid method",
+      });
+  }
 }
