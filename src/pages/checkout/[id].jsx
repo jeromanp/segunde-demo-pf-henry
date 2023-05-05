bf3d4606f04f50d95d9ae1fc4303615ee20565cb
Marco <tignanellimarco@gmail.com>
2023-05-05 18:19:51 -0300

    session checkout validation

--- src/pages/checkout/[id].jsx
+++ src/pages/checkout/[id].jsx
@@ -1,46 +1,52 @@
+import { useSession } from "@supabase/auth-helpers-react";
 import CheckOutForm from "components/CheckOutForm";
 import LayoutMain from "layouts/Layout";
-import { useState } from "react";
+import { useRouter } from "next/router";
+import Login from "pages/login";
 import { supabase } from "utils/supabase";
 
 export default function CheckOut({ room }) {
-    //Para controlar si la pagina no termino de cargar
-    const [isLoading, setIsLoading] = useState(true);
+    const session = useSession();
+    const router = useRouter();
 
     const mock = {
         price: 19,
         night: 7,
         extra: 20,
     };
+
     return (
         <>
-            <LayoutMain>
-                <div className="flex flex-col md:flex-row p-6 pb-12 items-start">
-                    <button
-                        className="flex items-center text-brand-green font-bold pr-4 pl-6"
-                        onClick={() => window.history.back()}
-                    >
-                        <img
-                            src="/arrowBack.svg"
-                            alt=""
-                            className="w-4/5 pt-3.5"
+        {session ?
+                <LayoutMain>
+                    <div className="flex flex-col md:flex-row p-6 pb-12 items-start">
+                        <button
+                            className="flex items-center text-brand-green font-bold pr-4 pl-6"
+                            onClick={() => window.history.back()}
+                        >
+                            <img
+                                src="/arrowBack.svg"
+                                alt=""
+                                className="w-4/5 pt-3.5"
+                            />
+                        </button>
+                        <CheckOutForm
+                            name={room.name}
+                            price={mock.price}
+                            night={mock.night}
+                            extra={mock.extra}
                         />
-                    </button>
-                    <CheckOutForm
-                        name={room.name}
-                        price={mock.price}
-                        night={mock.night}
-                        extra={mock.extra}
-                    />
-                    <img
-                        src="/ilustrationCheck.svg"
-                        alt=""
-                        className="w-5/12 mr-16 ml-auto self-center"
-                    />
-                </div>
-            </LayoutMain>
-        </>
-    );
+                        <img
+                            src="/ilustrationCheck.svg"
+                            alt=""
+                            className="w-5/12 mr-16 ml-auto self-center"
+                        />
+                    </div>
+                </LayoutMain>
+            : <Login /> }
+            </>
+        );
+    
 }
 
 export async function getServerSideProps({ params }) {
