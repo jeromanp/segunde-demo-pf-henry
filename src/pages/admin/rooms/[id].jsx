ded696170031df8678f9957c228dd6247ac01f22
Marco <tignanellimarco@gmail.com>
2023-05-06 16:31:56 -0300

    room edit funcional

--- src/pages/admin/rooms/[id].jsx
+++ src/pages/admin/rooms/[id].jsx
@@ -1,12 +1,12 @@
 import Layout from '../../../layouts/DashboardLayout'
 import Header from '../../../components/dashboard/PageHeader'
 import RoomForm from 'components/form/admin/RoomForm'
+import { supabase } from 'utils/supabase'
 
-export default function Edit()
-{
+export default function Edit({ room }) {
 	return (
 		<Layout>
-			<Header 
+			<Header
 				title="Edit cabaña"
 				breadcrumbs={(<>
 					<li>/</li>
@@ -18,7 +18,7 @@ export default function Edit()
 				<div className="col-span-5 xl:col-span-3 w-1/2">
 					<div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
 						<div className="p-7">
-							<RoomForm />
+							<RoomForm room={room} />
 						</div>
 					</div>
 				</div>
@@ -26,3 +26,24 @@ export default function Edit()
 		</Layout>
 	)
 }
+
+export async function getServerSideProps({ params }) {
+	const { id } = params;
+  
+	const { data: room, error } = await supabase
+	  .from("rooms")
+	  .select("*")
+	  .eq("id", id);
+  
+	if (error) {
+	  return {
+		notFound: true,
+	  };
+	}
+  
+	return {
+	  props: {
+		room: room[0],
+	  },
+	};
+  }
