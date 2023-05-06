4d28e13aff38c02eb1251bc19639dabca6573fdf
jestruvec <jhonnyestruve@gmail.com>
2023-05-06 12:20:27 -0600

    front end 74: deshardcodear admin/booking/index

--- src/pages/admin/booking/index.jsx
+++ src/pages/admin/booking/index.jsx
@@ -1,85 +1,118 @@
-import Layout from '../../../layouts/DashboardLayout'
-import Header from '../../../components/dashboard/PageHeader'
-import TableHead from '../../../components/dashboard/tables/TableHead'
-import Link from 'next/link'
+import Layout from "../../../layouts/DashboardLayout";
+import Header from "../../../components/dashboard/PageHeader";
+import TableHead from "../../../components/dashboard/tables/TableHead";
+import Link from "next/link";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const table_head = [
-	{ idx: 'date', title: 'Fecha', width: '220px' },
-	{ idx: 'cabin', title: 'Cabaña', width: '150px' },
-	{ idx: 'check-in', title: 'Check-in', width: '120px' },
-	{ idx: 'check-out', title: 'Check-out', width: '120px' },
-	{ idx: 'actions', title: 'Acciones' },
-]
-
+  { idx: "date", title: "Fecha", width: "220px" },
+  { idx: "cabin", title: "Cabaña", width: "150px" },
+  { idx: "check-in", title: "Check-in", width: "120px" },
+  { idx: "check-out", title: "Check-out", width: "120px" },
+  { idx: "actions", title: "Acciones" },
+];
 
 export default function Dashboard() {
+  const [bookings, setBookings] = useState([]);
+  const [rooms, setRooms] = useState([]);
 
-	return (
-		<Layout>
-			<Header
-				title="Reservas"
-				breadcrumbs={(<>
-					<li>/</li>
-					<li className="text-primary">Reservas</li>
-				</>)}>
+  useEffect(() => {
+    axios
+      .get("/api/booking")
+      .then((response) => {
+        setBookings(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    axios
+      .get("/api/cabanas")
+      .then((response) => {
+        setRooms(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-				<Link
-					href="/admin/booking/create"
-					className="inline-flex items-center justify-center rounded-md bg-primary bg-opacity-70 py-1.5 px-4 text-sm text-center font-medium text-white hover:bg-opacity-90">
-					Nueva reserva
-				</Link>
-			</Header>
+  const handleRoom = (id) => {
+    const room = rooms.find((room) => room.id === id);
+    return room.name;
+  };
 
+  return (
+    <Layout>
+      <Header
+        title="Reservas"
+        breadcrumbs={
+          <>
+            <li>/</li>
+            <li className="text-primary">Reservas</li>
+          </>
+        }
+      >
+        <Link
+          href="/admin/booking/create"
+          className="inline-flex items-center justify-center rounded-md bg-primary bg-opacity-70 py-1.5 px-4 text-sm text-center font-medium text-white hover:bg-opacity-90"
+        >
+          Nueva reserva
+        </Link>
+      </Header>
 
-			<div className="flex flex-col gap-10">
-				<div
-					className="rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default  sm:px-7.5 xl:pb-1">
-					<div className="max-w-full overflow-x-auto">
+      <div className="flex flex-col gap-10">
+        <div className="rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default  sm:px-7.5 xl:pb-1">
+          <div className="max-w-full overflow-x-auto">
+            <table className="w-full table-auto">
+              <TableHead data={table_head} />
 
-						<table className="w-full table-auto">
-							<TableHead data={table_head} />
-
-							<tbody>
-								{[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(e => (
-									<tr key={e}>
-										<td className="border-b border-[#eee] py-5 px-4">
-											<p className="text-black">Abr. 12, 2023</p>
-										</td>
-										<td className="border-b border-[#eee] py-5 px-4">
-											<h5 className="font-medium text-black">Cabaña A</h5>
-											<p className="text-sm">La mas mejor</p>
-										</td>
-										<td className="border-b border-[#eee] py-5 px-4">
-											<p className="text-black">May 13, 2023</p>
-										</td>
-										<td className="border-b border-[#eee] py-5 px-4">
-											<p className="text-black">May 18, 2023</p>
-										</td>
-										<td className="border-b border-[#eee] py-5 px-4">
-											<div className="flex items-center space-x-3.5">
-												<Link className="hover:text-primary" href="detail-reserva.html">
-													<i className="ri-file-text-line text-xl leading-none"></i>
-												</Link>
-												<a className="hover:text-primary" href="/admin/booking/edit">
-													<i className="ri-edit-line text-xl leading-none"></i>
-												</a>
-												<button
-													// onClick={deleteHandler}
-													className="hover:text-primary ri-close-circle-line text-xl leading-none"
-												// value={room.id}
-												>
-												</button>
-
-											</div>
-										</td>
-									</tr>
-								))}
-							</tbody>
-						</table>
-
-					</div>
-				</div>
-			</div>
-		</Layout>
-	)
+              <tbody>
+                {bookings &&
+                  bookings.map((booking) => (
+                    <tr key={booking.id}>
+                      <td className="border-b border-[#eee] py-5 px-4">
+                        <p className="text-black">{booking.created_at}</p>
+                      </td>
+                      <td className="border-b border-[#eee] py-5 px-4">
+                        <h5 className="font-medium text-black">
+                          {handleRoom(booking.room_id)}
+                        </h5>
+                        <p className="text-sm">Lorem ipsum dolor sit.</p>
+                      </td>
+                      <td className="border-b border-[#eee] py-5 px-4">
+                        <p className="text-black">{booking.checkin}</p>
+                      </td>
+                      <td className="border-b border-[#eee] py-5 px-4">
+                        <p className="text-black">{booking.checkout}</p>
+                      </td>
+                      <td className="border-b border-[#eee] py-5 px-4">
+                        <div className="flex items-center space-x-3.5">
+                          <Link
+                            className="hover:text-primary"
+                            href="detail-reserva.html"
+                          >
+                            <i className="ri-file-text-line text-xl leading-none"></i>
+                          </Link>
+                          <a
+                            className="hover:text-primary"
+                            href="/admin/booking/edit"
+                          >
+                            <i className="ri-edit-line text-xl leading-none"></i>
+                          </a>
+                          <button
+                            // onClick={deleteHandler}
+                            className="hover:text-primary ri-close-circle-line text-xl leading-none"
+                            // value={room.id}
+                          ></button>
+                        </div>
+                      </td>
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 }
