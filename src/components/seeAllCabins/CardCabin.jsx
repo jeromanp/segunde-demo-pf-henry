0630465304937943d72376a4a78d79fcdcd3751e
Marco <tignanellimarco@gmail.com>
2023-05-08 09:50:42 -0300

    link reposition & text fix

--- src/components/seeAllCabins/CardCabin.jsx
+++ src/components/seeAllCabins/CardCabin.jsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 
 export default function CardCabin({ cabin }) {
     return (
+        <Link href={`/cabanas/${cabin.id}`}>
         <div className="border rounded-2xl overflow-hidden shadow-lg flex flex-col md:flex-row m-1">
             <div className="relative w-80 h-56 flex-shrink-0 p-4">
                 <img
@@ -13,14 +14,14 @@ export default function CardCabin({ cabin }) {
                 />
             </div>
             <div className="pl-8 pr-4 pt-6">
-                <h2 className="text-brand-green text-2xl font-semibold mb-4 hover:underline">
-                    <Link href={`/cabanas/${cabin.id}`}>{cabin.name}</Link>
+                <h2 className="text-brand-green text-2xl font-semibold mb-4">
+                    {cabin.name}
                 </h2>
                 <div className="text-lg mb-0.5">
                     <i className="ri-user-3-fill text-gray-500 mr-2"></i>
                     <span className="text-gray-800 font-normal">
                         Cabaña de {cabin.rooms} habitaciones para
-                        {cabin.capacity} personas
+                        {` ${cabin.capacity}`} personas
                     </span>
                 </div>
                 {cabin.services &&
@@ -54,6 +55,8 @@ export default function CardCabin({ cabin }) {
                         </div>
                     )}
             </div>
+            
         </div>
+        </Link>
     );
 }
