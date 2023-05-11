2a26dd24e2fe23ecca76a0382ba5f347d94cf008
Marco <tignanellimarco@gmail.com>
2023-05-11 18:28:55 -0300

    functionality form

--- src/pages/opinion.jsx
+++ src/pages/opinion.jsx
@@ -1,70 +1,91 @@
-import { useSession } from "@supabase/auth-helpers-react";
+import axios from "axios";
+import Layout from "layouts/Layout";
 import { useState } from "react";
+import { useSession } from "@supabase/auth-helpers-react";
+import { getProfileId } from "helpers/dbHelpers.js";
+import Swal from "sweetalert2";
 
-const RatingForm = () => {
+export default function RatingForm() {
   const [rating, setRating] = useState(0);
   const [comment, setComment] = useState("");
   const session = useSession();
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log(`Rating: ${rating} stars\nComment: ${comment}`);
+
+    axios.post('/api/comments', {
+      review: comment,
+      stars: rating,
+      user_id: (await getProfileId(session.user.id)).userId,
+    })
+      .then(response => {
+        Swal.fire(
+          'Se envío tu comentario',
+          '¡Ojalá vuelvas pronto!',
+          'success'
+        )
+      })
+      .catch(error => {
+        Swal.fire(
+          'Hubo un error en el envío de tu comentario',
+          'Porfavor, intenta de nuevo más tarde',
+          'error'
+        )
+      })
   };
 
   return (
-    <form
-    onSubmit={handleSubmit}
-    className="my-4 flex flex-col items-center"
-  >
-    <div className="text-center">
-      <h1 className="text-3xl font-bold text-brand-green">
-        ¡Cuéntanos cómo te pareció tu estadía!
-      </h1>
-    </div>
-    <div className="my-10 w-full max-w-xl">
-      <label className="block font-medium text-3xl text-brand-light-green mb-2 text-center">
-        Califica tu experiencia
-      </label>
-      <div className="flex justify-center my-2">
-        {[1, 2, 3, 4, 5].map((star) => (
-          <button
-            key={star}
-            type="button"
-            className={`text-4xl ${
-              star <= rating ? "text-brand-yellow" : "text-brand-cream"
-            } mx-2`}
-            onClick={() => setRating(star)}
-          >
-            <i className="ri-star-fill" />
-          </button>
-        ))}
-      </div>
-    </div>
-    <div className="w-full max-w-xl">
-      <div className="border-2 rounded-3xl border-brand-light-green shadow-lg p-6">
-        <label className="block font-medium text-3xl text-brand-light-green text-center mb-4">
-          Deja tu comentario
-        </label>
-        <textarea
-          className="block w-full p-2 border border-brand-brown placeholder-brand-brown rounded-lg focus:border-brand-light-green focus:outline-none"
-          style={{ height: "100px" }}
-          value={comment}
-          onChange={(event) => setComment(event.target.value)}
-          placeholder="Me pareció..."
-        />
-      </div>
-    </div>
-    <div className="pt-6 w-full max-w-xl">
-      <button
-        type="submit"
-        className="bg-brand-yellow text-white px-10 py-2 rounded-lg w-full"
+    <Layout>
+      <form
+        onSubmit={handleSubmit}
+        className="my-10 flex flex-col items-center mx-8"
       >
-        Enviar
-      </button>
-    </div>
-  </form>
-  
+        <div className="text-center">
+          <h1 className="text-3xl font-bold text-brand-green">
+            ¡Cuéntanos cómo te pareció tu estadía!
+          </h1>
+        </div>
+        <div className="my-10 w-full max-w-xl">
+          <label className="block font-medium text-3xl text-brand-light-green mb-2 text-center">
+            Califica tu experiencia
+          </label>
+          <div className="flex justify-center my-2">
+            {[1, 2, 3, 4, 5].map((star) => (
+              <button
+                key={star}
+                type="button"
+                className={`text-4xl ${star <= rating ? "text-brand-yellow" : "text-brand-cream"
+                  } mx-2`}
+                onClick={() => setRating(star)}
+              >
+                <i className="ri-star-fill" />
+              </button>
+            ))}
+          </div>
+        </div>
+        <div className="w-full max-w-xl">
+          <div className="border-2 rounded-3xl border-brand-light-green shadow-lg p-6">
+            <label className="block font-medium text-3xl text-brand-light-green text-center mb-4">
+              Deja tu comentario
+            </label>
+            <textarea
+              className="resize-none block w-full p-2 border border-brand-brown placeholder-brand-brown rounded-lg focus:border-brand-light-green focus:outline-none"
+              style={{ height: "150px" }}
+              value={comment}
+              onChange={(event) => setComment(event.target.value)}
+              placeholder="Me pareció..."
+            />
+          </div>
+        </div>
+        <div className="pt-6 w-full max-w-xl">
+          <button
+            type="submit"
+            className="bg-brand-yellow text-white px-10 py-2 rounded-lg w-full"
+          >
+            Enviar
+          </button>
+        </div>
+      </form>
+    </Layout>
   );
 };
-
-export default RatingForm;
