fba2e0d3202be8a113463d27146d3e5e2d9180f6
Santi-Teran <santiagoteranmatias@gmail.com>
2023-05-11 10:49:53 -0300

    vista de detalles de reserva

--- src/pages/admin/booking/[id].jsx
+++ src/pages/admin/booking/[id].jsx
@@ -1,6 +1,6 @@
 import Layout from '../../../layouts/DashboardLayout'
 import Header from '../../../components/dashboard/PageHeader'
-import BookingForm from 'components/form/admin/BookingForm'
+// import BookingForm from 'components/form/admin/BookingForm'
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import dayjs from 'dayjs'
@@ -38,49 +38,49 @@ useEffect(() => {
             </div> */}
 					  {loading 
               ? ( <div className="text-center">Loading...</div> ) 
-              : ( <div className='flex flex-col p-4 mx-auto w-full'>
+              : ( <div className='flex flex-col px-20 py-24 mx-auto w-full'>
 								    <div className="flex md:flex-row flex-col mb-8">
                       <div className="w-1/2">
                         <h3 className="text-lg font-bold text-primary">Nombre:</h3>
-                        <p className='ml-4'>{booking.profiles.full__name ? booking.profiles.full__name : '-'}</p>
+                        <p>{booking.profiles.full__name ? booking.profiles.full__name : '-'}</p>
                       </div>
 									    <div className="w-1/2">
                         <h3 className="text-lg font-bold text-primary">Email:</h3>
-                        <p className='ml-4'>{booking.profiles.email}</p>
+                        <p>{booking.profiles.email}</p>
                       </div>
                     </div>
                     <div className="flex md:flex-row flex-col mb-8">
                       <div className="w-1/2">
                         <h3 className="text-lg font-bold text-primary">Cabana:</h3>
-                        <p className='ml-4'>{booking.rooms.name}</p>
+                        <p>{booking.rooms.name}</p>
                       </div>
 									    <div className="w-1/2">
                         <h3 className="text-lg font-bold text-primary">Huespedes:</h3>
                         <ul className='flex'>
-                          <li className='ml-4'>Adultos: {booking.adults}</li>
-											    <li li className='ml-4'>-</li>
-                          <li className='ml-4'>Ninos: {booking.children ? booking.children : '0'}</li>
+                          <li>Adultos: {booking.adults}</li>
+											    <li>-</li>
+                          <li>Ninos: {booking.children ? booking.children : '0'}</li>
                         </ul>
                       </div>
                     </div>
                     <div className="flex md:flex-row flex-col mb-8">
                       <div className="w-1/2">
                         <h3 className="text-lg font-bold text-primary">Check In:</h3>
-                        <p className='ml-4'>{ dayjs(booking.checkin).format('DD MMM, YYYY') }</p>
+                        <p>{ dayjs(booking.checkin).format('DD MMM, YYYY') }</p>
                       </div>
                       <div className="w-1/2">
                         <h3 className="text-lg font-bold text-primary">Check Out:</h3>
-                        <p className='ml-4'>{ dayjs(booking.checkout).format('DD MMM, YYYY') }</p>
+                        <p>{ dayjs(booking.checkout).format('DD MMM, YYYY') }</p>
                       </div>
                     </div>
                     <div className="flex md:flex-row flex-col mb-8">
 									    <div className="w-1/2">
                         <h3 className="text-lg font-bold text-primary">Pagado:</h3>
-                        <p className='ml-4'>{booking.payments ? 'Si' : 'No'}</p>
+                        <p>{booking.payments ? 'Si' : 'No'}</p>
                       </div>
 									    <div className="w-1/2">
                         <h3 className="text-lg font-bold text-primary">Suspendido:</h3>
-                        <p className='ml-4'>{booking.suspended ? 'Si' : 'No'}</p>
+                        <p>{booking.suspended ? 'Si' : 'No'}</p>
                       </div>
                     </div>
                   </div>
