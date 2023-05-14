67acb6c3cca3ccde9aaf784ac36c9ef17f0f4a84
Lafsitoo <fretteleandro@live.com>
2023-05-13 21:24:15 -0300

    screen

--- src/pages/servicios.jsx
+++ src/pages/servicios.jsx
@@ -30,7 +30,7 @@ export default function Services(){
 
 	return (
 		<Layout>
-			<div className="container mx-auto px-6 2xl:px-0">
+			<div className="container h-screen mx-auto px-6 2xl:px-0">
 				<h2 
 					className="text-brand-green text-3xl font-bold 
 											leading-none text-center pt-14 pb-8 
